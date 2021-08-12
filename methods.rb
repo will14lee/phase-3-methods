63587e45ed8acdc4dd471aa5b5be448340073969
will14lee <will14lee@gmail.com>
2021-08-12 18:14:37 -0400

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
-# Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default name= "programmer"
+    puts "Hello, #{name}!"
+end   
+
+greet_with_default
+
+greet_with_default "Sunny"
+
+def add num1, num2
+    num1 + num2
+end
+add 1,2
+
+def halve number
+    if number.class != Integer
+        puts number.class
+        return nil
+    end
+    number/2
+end
